1791aaacb239e6b04d9c99b6def8007401cff6ad
Praveen <praveenme117@gmail.com>
2026-07-17 20:24:39 +0530

    update chat

--- ai-backend/app/services/llm_service.py
+++ ai-backend/app/services/llm_service.py
@@ -1,4 +1,5 @@
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
 from app.config.settings import settings
 import logging
 
@@ -13,10 +14,35 @@ class LLMService:
             temperature=0,
         )
 
+        self.prompt=ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    """ You are a Senior Backend Engineer.
+                    Explain concepts clearly.
+                    Prefer Java Springboot and node js, typescript examples.
+                    Keep answers concise.
+                    """,
+                ),
+                (
+                    "human",
+                    "{question}",
+                ),
+            ]
+        )
+
         logger.info("Groq client initialized succesfully")
+    
 
     def chat(self, message:str) -> str:
         logger.info("Received prompt: %s", message)
-        response = self.llm.invoke(message)
+
+        messages=self.prompt.invoke(
+            {
+                "question":message
+            }
+        )
+        logger.info(messages)
+        response = self.llm.invoke(messages)
         logger.info("Received response from Groq.")
         return response.content
